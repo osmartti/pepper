cb7096653971a21f78f351c1b40a099d0ed7a930
Leevi Vesa <joulumieli2@gmail.com>
2018-09-07 14:28:41 +0300

    Initial Commit

---
+++ Git test/translations/translation_en_US.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello 1 human</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello 1 human</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation>Hello</translation>
+        </message>
+    </context>
+</TS>
