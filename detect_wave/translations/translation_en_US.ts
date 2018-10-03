3c6ea6e7f53a297b69a3dc361f2234899640a2f2
Juha Veijalainen <pahakuningas@gmail.com>
2018-10-03 14:38:02 +0300

    detect and wave

---
+++ detect_wave/translations/translation_en_US.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation>Hello</translation>
+        </message>
+        <message>
+            <source>detecrted</source>
+            <comment>Text</comment>
+            <translation type="obsolete">detecrted</translation>
+        </message>
+        <message>
+            <source>kill all humans</source>
+            <comment>Text</comment>
+            <translation type="obsolete">kill all humans</translation>
+        </message>
+        <message>
+            <source>testti </source>
+            <comment>Text</comment>
+            <translation type="obsolete">testti </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline/behavior_layer1/keyframe1/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>COME HERE</source>
+            <comment>Text</comment>
+            <translation type="obsolete">COME HERE</translation>
+        </message>
+    </context>
+</TS>
